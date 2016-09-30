d9dedb4831e2f129ad6940c22a688996110f2da6
Garrett Boggs <garrett.boggs@my.wheaton.edu>
2016-09-30 11:33:59 -0700

    sighting passes all test

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -11,24 +11,73 @@ public class Sighting {
   private Timestamp sightTime;
   private int animalid;
 
-  public Sighting(String location, String rangerName, Timestamp sightTime, int animalid){
+  public Sighting(String location, String rangerName, int animalid){
     this.location = location;
     this.rangerName = rangerName;
-    this.sightTime = sightTime;
     this.animalid = animalid;
+  }
+
+  public int getId(){
+    return id;
+  }
+
+  public String getLocation(){
+    return location;
+  }
+
+  public String getRangerName(){
+    return rangerName;
+  }
+
+  public int getAnimalId(){
+    return animalid;
+  }
+
+  public Timestamp getSightTime(){
+    return sightTime;
   }
 
   public void save() {
     try(Connection con = DB.sql2o.open()) {
-      String sql = "INSERT INTO sightings (location, ranger_name, sight_time, animalid) VALUES (:location, :rangerName, now(), :animalid";
+      String sql = "INSERT INTO sightings(location, animalid, ranger_name, sight_time) VALUES (:location, :animalid, :ranger_name, now() );";
       this.id = (int) con.createQuery(sql, true)
         .addParameter("location", this.location)
-        .addParameter("rangerName", this.rangerName)
         .addParameter("animalid", this.animalid)
+        .addParameter("ranger_name", this.rangerName)
+        .throwOnMappingFailure(false)
         .executeUpdate()
         .getKey();
     }
   }
 
+    public static List<Sighting> all() {
+     String sql = "SELECT * FROM sightings";
+     try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql)
+      .throwOnMappingFailure(false)
+      .executeAndFetch(Sighting.class);
+     }
+   }
 
+   @Override
+   public boolean equals(Object otherSighting){
+     if (!(otherSighting instanceof Sighting)) {
+       return false;
+     } else {
+       Sighting newSighting = (Sighting) otherSighting;
+       return this.getLocation().equals(newSighting.getLocation()) &&
+              this.getId() == newSighting.getId() &&
+              this.getAnimalId() == newSighting.getAnimalId();
+     }
+   }
+
+   public static Sighting find(int id) {
+     try(Connection con = DB.sql2o.open()) {
+       String sql = "SELECT * FROM sightings where id=:id";
+       Sighting sighting = con.createQuery(sql)
+         .addParameter("id", id)
+         .executeAndFetchFirst(Sighting.class);
+       return sighting;
+     }
+   }
 }
--- src/test/java/SightingTest.java
+++ src/test/java/SightingTest.java
@@ -6,9 +6,89 @@ import java.time.LocalDateTime;
 
 public class SightingTest {
 
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+  
+  @Test
   public void sighting_instantiatesCorrectly_true() {
-    EndangeredAnimal testEndangeredAnimal = new EndangeredAnimal("Deer", "sick", "young");
-    assertEquals(true, testEndangeredAnimal instanceof EndangeredAnimal);
+    Sighting testSighting = new Sighting("River", "Steve" , 1);
+    assertEquals(true, testSighting instanceof Sighting);
   }
 
-}
+  @Test
+   public void Sighting_instantiatesCorrectly_true() {
+     Sighting mySighting = new Sighting("River", "Steve", 1);
+     assertEquals(true, mySighting instanceof Sighting);
+   }
+
+   @Test
+   public void Sighting_instantiatesWithLocation_String() {
+     Sighting mySighting = new Sighting("River", "Steve", 1);
+     assertEquals("River", mySighting.getLocation());
+   }
+
+   /* @Test
+   public void getCreatedAt_instantiatesWithCurrentTime_today() {
+     Sighting mySighting = new Sighting("River", "Steve", 1);
+     assertEquals(LocalDateTime.now().getDayOfWeek(), mySighting.getCreatedAt().getDayOfWeek());
+   } */
+
+   @Test
+   public void all_returnsAllInstancesOfSighting_true() {
+     Sighting firstSighting = new Sighting("River", "Steve", 1);
+     firstSighting.save();
+     Sighting secondSighting = new Sighting("Volcano", "Zelda", 1);
+     secondSighting.save();
+     assertEquals(true, Sighting.all().get(0).equals(firstSighting));
+     assertEquals(true, Sighting.all().get(1).equals(secondSighting));
+   }
+
+   @Test
+   public void getId_tasksInstantiateWithAnID_1() {
+     Sighting mySighting = new Sighting("River", "Steve", 1);
+     mySighting.save();
+     assertTrue(mySighting.getId() > 0);
+   }
+
+   @Test
+   public void find_returnsSightingWithSameId_secondSighting() {
+     Sighting firstSighting = new Sighting("River", "Steve", 1);
+     firstSighting.save();
+     Sighting secondSighting = new Sighting("Volcano", "Zelda", 1);
+     secondSighting.save();
+     assertEquals(Sighting.find(secondSighting.getId()), secondSighting);
+   }
+
+   @Test
+   public void equals_returnsTrueIfLocationsAretheSame() {
+     Sighting firstSighting = new Sighting("River", "Steve", 1);
+     Sighting secondSighting = new Sighting("River", "Steve", 1);
+     assertTrue(firstSighting.equals(secondSighting));
+   }
+
+   @Test
+   public void save_returnsTrueIfLocationsAretheSame() {
+     Sighting mySighting = new Sighting("River", "Steve", 1);
+     mySighting.save();
+     assertTrue(Sighting.all().get(0).equals(mySighting));
+   }
+
+   @Test
+   public void save_assignsIdToObject() {
+     Sighting mySighting = new Sighting("River", "Steve", 1);
+     mySighting.save();
+     Sighting savedSighting = Sighting.all().get(0);
+     assertEquals(mySighting.getId(), savedSighting.getId());
+   }
+
+   @Test
+   public void save_savesAnimalIdIntoDB_true() {
+     Animal myAnimal = new Animal("Dragon");
+     myAnimal.save();
+     Sighting mySighting = new Sighting("River", "Steve", myAnimal.getId());
+     mySighting.save();
+     Sighting savedSighting = Sighting.find(mySighting.getId());
+     assertEquals(savedSighting.getAnimalId(), myAnimal.getId());
+   }
+
+ }
